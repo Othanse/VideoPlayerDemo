6449177de69511da9da978b962e1125edc68b741
WangShuai <shuai.wang@appshahe.com>
2019-02-14 15:27:50 +0800

    添加部分描述

--- app/src/main/java/xyz/eagleweb/videodemo/listplayer/SplashActivity.java
+++ app/src/main/java/xyz/eagleweb/videodemo/listplayer/SplashActivity.java
@@ -10,8 +10,12 @@ public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        // 选用节操播放器做的。列表自动播放
         startActivity(new Intent(this, JcvdMainActivity.class));
+        // 选用GSY播放器做的。列表自动播放
         //        startActivity(new Intent(this, GSYMainActivity.class));
         finish();
+        // TODO: 2019/2/14 客观的说 GSY播放器比节操播放器要功能全面和强大一些。但是包体积也是大了很多！需要取舍！  JC不大 一百多K！ GSY最小也需要1.2M
+        // TODO: 2019/2/14 可以利用节操播放器的UI + IjkPlayer的内核 能基本达到GSY的功能
     }
 }
